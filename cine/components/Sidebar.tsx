6c40b84e9dab4ea44ab5b7c3aa48f7f9f412f5ea
Sayo Adegoroye <steve.legion94@gmail.com>
2024-05-06 22:17:25 +0100

    Update Sidebar.tsx

--- cine/components/Sidebar.tsx
+++ cine/components/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
   const pathname = usePathname();
   return (
     <section className="sticky left-0 flex top-0 h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
-      <div className="flex flex-col flex- gap-6">
+      <div className="flex flex-col flex-1 gap-6">
         {sidebarLinks.map((link) => {
           const isActive =
             pathname === link.route || pathname.startsWith(link.route);

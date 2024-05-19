1bb6005572d8092c17e6ea503c058f8c4d0b6a05
Sayo Adegoroye <steve.legion94@gmail.com>
2024-05-19 22:16:18 +0100

    Update stream.actions.ts

--- cine/actions/stream.actions.ts
+++ cine/actions/stream.actions.ts
@@ -24,3 +24,98 @@ export const tokenProvider = async () => {
 
   return token;
 };
+
+// <script src="https://cdn.jsdelivr.net/npm/@finsweet/cms-library@1/cms-library.js"></script>
+
+// <script>
+// (function() {
+//   const companyCollection = new FsLibrary('.company-collection-list')
+
+// const filters = [
+//   {
+//     filterWrapper: ".existence-filter-wrapper",
+//     filterType: "exclusive"
+//   },
+// {
+//     filterWrapper: ".industry-filter-wrapper",
+//     filterType: "exclusive"
+//   },
+// ]
+
+//   companyCollection.filter({
+//     filterArray: filters,
+//     activeClass: 'company-filter-active',
+//     emptyMessage: '.empty-message',
+//     animation: {
+//       enable: true,
+//       duration: 200,
+//       easing: 'ease-out',
+//       effects: 'fade translate(0px,20px)'
+//     }
+
+//   })
+// })();
+
+// //Initial filter function - ensures active states
+// (function() {
+// 	const defaultFilters = document.querySelectorAll('.default-company-filter')
+//   defaultFilters.forEach(element => element.click())
+// })();
+
+// //Function to remove purple bg from the 'Exits' companies
+// (function() {
+// let companies = document.querySelectorAll('.company-active')
+// 	Array.from(companies).forEach((company, i) => {
+//     if(company.innerHTML.toLowerCase() === 'full exits') {
+//     let parentDiv = company.parentElement
+//     parentDiv.style.backgroundColor = '#D9D9D9'
+
+//     //let companyLogo = parentDiv.getElementsByTagName('img')[0]
+//     //Exits company logo should be grey
+//     //companyLogo.classList.add('grey-img');
+//   }
+
+//   if(company.innerHTML.toLowerCase() === 'partial exits') {
+//     let parentDiv = company.parentElement
+//     parentDiv.style.backgroundColor = '#AA39E3'
+
+//   }
+
+// });
+// })()
+
+// </script>
+
+// <script>
+// document.addEventListener("DOMContentLoaded", function() {
+//   const toggleButtons = document.querySelectorAll(".toggle-button");
+
+//   toggleButtons.forEach(toggleButton => {
+//     toggleButton.addEventListener("click", function() {
+//       const detailContent = this.nextElementSibling;
+//       const gridItem = this.closest(".portco-collection-item");
+//       const gridHeight = gridItem.offsetHeight;
+//       const gridGap = parseInt(window.getComputedStyle(gridItem).marginBottom);
+
+//       detailContent.style.top = `${gridHeight + gridGap}px`;
+//     });
+//   });
+// });
+// </script>
+
+// <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+// <style>
+// .industry-filter-select {
+//     -webkit-appearance: none;
+//     -moz-appearance: none;
+//     text-indent: 1px;
+//     text-overflow: '';
+// }
+
+// .grey-img {
+//   filter: contrast(0.1);
+//   -webkit-filter: contrast(0.1);
+//   filter: contrast(0.1);
+// }
+// </style>
